5974676ac6c11dc6bef9bd3a275e57fc7087bfa8
Manivannan <smoneymca@gmail.com>
2020-10-05 00:54:08 +0530

    Add files via upload

---
+++ OptumAllianceAPI/Models/Appointment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OptumAllianceAPI.Models
+{
+    public class Appointment
+    {
+        public string MemberName { get; set; }
+        public string MemberID { get; set; }
+        public DateTime ScheduledDate { get; set; }
+        public string Gender { get; set; }
+        public DateTime? MemberDOB { get; set;}
+
+    }
+}
